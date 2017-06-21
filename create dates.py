bbe0a6daee54b42cbc96a6203ede9adf2d594d65
hemantmshah26 <hemant.m.shah@gmail.com>
2017-06-21 15:08:54 -0700

    Update create dates.py

--- create dates.py
+++ create dates.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
 # Below we define the range of dates we want 
-dates = pd.date_range(20160501, periods = 10)
+dates = pd.date_range('20160501', periods = 10)
 dates
