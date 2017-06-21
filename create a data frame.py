bc2058622f8bcacdc57756ec2f2615dfb456cee1
hemantmshah26 <hemant.m.shah@gmail.com>
2017-06-21 15:09:14 -0700

    Update create a data frame.py

--- create a data frame.py
+++ create a data frame.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 # define a date range 
-dates = pd.date_range(20160501, periods = 10)
+dates = pd.date_range('20160501', periods = 10)
 dates
 # create a dataframe with above 10 dates as index and colums defined as ABCDE
 dataframe = pd.DataFrame(np.random.randn (10,5), index = dates, columns =list ('ABCDE'))
